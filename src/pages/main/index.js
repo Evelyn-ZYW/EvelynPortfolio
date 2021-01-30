ead91d9db00304a064b28bdcf3eb9eec7ed570a0
Evelyn <evelynzdragon@gmail.com>
2021-01-30 14:10:23 -0800

    more setup

---
+++ src/pages/main/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Main = () => {
+    return <div className="main">
+        This is the page for testings
+    </div>
+}
+
+export default Main;
